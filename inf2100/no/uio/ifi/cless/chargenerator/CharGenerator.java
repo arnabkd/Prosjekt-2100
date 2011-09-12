efa8413cb326ff0136df8f2da71dc278e71d014f
Arnab Kumar Datta <arnabkd@student.uio.no>
2011-09-12 16:02:09 +0200

    fixed indentation on some files

--- inf2100/no/uio/ifi/cless/chargenerator/CharGenerator.java
+++ inf2100/no/uio/ifi/cless/chargenerator/CharGenerator.java
@@ -13,45 +13,45 @@ import no.uio.ifi.cless.log.Log;
  * Module for reading single characters.
  */
 public class CharGenerator {
-    public static char curC, nextC;
-	
-    private static LineNumberReader sourceFile = null;
-    private static String sourceLine;
-    private static int sourcePos;
-	
-    public static void init() {
-	try {
-	    sourceFile = new LineNumberReader(new FileReader(CLess.sourceName));
-	} catch (FileNotFoundException e) {
-	    Error.error("Cannot read " + CLess.sourceName + "!");
-	}
-	sourceLine = "";  sourcePos = 0;  curC = nextC = ' ';
-	readNext();  readNext();
-    }
-	
-    public static void finish() {
-	if (sourceFile != null) {
-	    try {
-		sourceFile.close();
-	    } catch (IOException e) {
-		Error.error("Could not close source file!");
-	    }
-	}
-    }
-	
-    public static boolean isMoreToRead() {
-	//-- Must be changed in part 0:
-	return false;
-    }
-	
-    public static int curLineNum() {
-	return (sourceFile == null ? 0 : sourceFile.getLineNumber());
-    }
-	
-    public static void readNext() {
-	curC = nextC;
-	if (! isMoreToRead()) return;
+	public static char curC, nextC;
 
-	//-- Must be changed in part 0:
-    }
+	private static LineNumberReader sourceFile = null;
+	private static String sourceLine;
+	private static int sourcePos;
+
+	public static void init() {
+		try {
+			sourceFile = new LineNumberReader(new FileReader(CLess.sourceName));
+		} catch (FileNotFoundException e) {
+			Error.error("Cannot read " + CLess.sourceName + "!");
+		}
+		sourceLine = "";  sourcePos = 0;  curC = nextC = ' ';
+		readNext();	 readNext();
+	}
+
+	public static void finish() {
+		if (sourceFile != null) {
+			try {
+				sourceFile.close();
+			} catch (IOException e) {
+				Error.error("Could not close source file!");
+			}
+		}
+	}
+
+	public static boolean isMoreToRead() {
+		//-- Must be changed in part 0:
+		return false;
+	}
+
+	public static int curLineNum() {
+		return (sourceFile == null ? 0 : sourceFile.getLineNumber());
+	}
+
+	public static void readNext() {
+		curC = nextC;
+		if (! isMoreToRead()) return;
+		
+		//-- Must be changed in part 0:
+	}
 }
