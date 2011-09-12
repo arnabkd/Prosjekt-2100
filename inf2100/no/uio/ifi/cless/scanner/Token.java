43d4233c86fdf96929fd7133d31e233fef63a6b6
Arnab Kumar Datta <arnabkd@student.uio.no>
2011-09-12 20:22:47 +0200

    Token class : - isOperatorToken(Token t) 	- isNumericalToken(Token t) 	- isComparisonOperator(Token t)

--- inf2100/no/uio/ifi/cless/scanner/Token.java
+++ inf2100/no/uio/ifi/cless/scanner/Token.java
@@ -8,18 +8,34 @@ package no.uio.ifi.cless.scanner;
  * The different kinds of tokens read by Scanner.
  */
 public enum Token { addToken, assignToken, commaToken, divideToken, elseToken,
-    eofToken, equalToken, forToken, greaterEqualToken, greaterToken, 
-    ifToken, intToken, leftBracketToken, leftCurlToken, leftParToken, 
-    lessEqualToken, lessToken, multiplyToken, nameToken, notEqualToken,
-    numberToken, rightBracketToken, rightCurlToken, rightParToken,
-    returnToken, semicolonToken, subtractToken, whileToken;
+		eofToken, equalToken, forToken, greaterEqualToken, greaterToken,
+		ifToken, intToken, leftBracketToken, leftCurlToken, leftParToken,
+		lessEqualToken, lessToken, multiplyToken, nameToken, notEqualToken,
+		numberToken, rightBracketToken, rightCurlToken, rightParToken,
+		returnToken, semicolonToken, subtractToken, whileToken;
 
-    public static boolean isOperand(Token t) {
-	return t==numberToken || t==nameToken || t==leftParToken;
-    }
+	public static boolean isOperand(Token t) {
+		return t==numberToken || t==nameToken || t==leftParToken;
+	}
 
-    public static boolean isOperator(Token t) {
-	//-- Must be changed in part 0:
-	return false;
-    }
+	public static boolean isOperator(Token t) {
+		//-- Must be changed in part 0:
+		return isNumberOperator(t) || isComparisonOperator(t);
+	}
+
+	public static boolean isNumberOperator(Token t) {
+		if(t == addToken) return true;
+		if(t == subtractToken) return true;
+		if(t == multiplyToken) return true;
+		if(t == divideToken) return true;
+	}
+
+	public static boolean isComparisonOperator(Token t) {
+		if(t == equalToken) return true;
+		if(t == notEqualToken) return true;
+		if(t == lessToken) return true;
+		if(t == lessEqualToken) return true;
+		if(t == greaterToken) return true;
+		if(t == greaterEqualToken) return true;
+	}
 }
